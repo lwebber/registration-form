f05af79a0efc0746697f50d761cfe01d642fe704
Laura Webber <laurawebber@Lauras-MacBook-Pro.local>
2020-08-02 11:00:37 -0700

    add pug structure

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
-const router = Express.router();
+const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("It works!");
+    res.render('form');
 });
 
 module.exports = router;
